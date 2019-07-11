054faec1c918ffeccbfaaa6c46a1732bc5dc4f37
faizashermuhammad <49517190+faizashermuhammad@users.noreply.github.com>
2019-07-11 17:48:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,86 @@
-let student={
-    name:"hira",
-    email:"hira@gmail.com",
-    address:"nishter road",
-}
+// let student={
+//     name:"hira",
+//     email:"hira@gmail.com",
+//     address:"nishter road",
+// }
 
-let student2={
-    name:"samreen",
-    address: "garden",
-    email:"gdh@gmail.com",
-}
+// let student2={
+//     name:"samreen",
+//     address: "garden",
+//     email:"gdh@gmail.com",
+// }
 
-let students=[
-    {
-        name:"amber",
-        addres: "garden",
-        age:19,
-        number:97654323 ,
-        email: "hgk@gail.com",
+// let students=[
+//     {
+//         name:"amber",
+//         addres: "garden",
+//         age:19,
+//         number:97654323 ,
+//         email: "hgk@gail.com",
     
 
-    name:"tuba",
-        addres: "garden",
-        age:19,
-        number:97654323 ,
-        email: "hgk@gail.com",
-}
+//     name:"tuba",
+//         addres: "garden",
+//         age:19,
+//         number:97654323 ,
+//         email: "hgk@gail.com",
+// }
+// ]
+
+
+let techkaro={
+courseName: "Tech Karo" ,
+students:[
+    {
+        name:
+        profession:
+        hobies:
+        contact:
+        address:
+        email:
+        qualifiction:
+        gender:
+        dob:
+        rollNo:
+        group:
+        fatherName:
+        fatherOccupation:
+        maritalStudent:
+        fatherIncome:
+        personalIncome:
+        noOfSiblings:
+        spouseOccupation:
+        heardForm:
+        extraSkills:
+        cnic:
+        isEmployed:
+        employementInfo:
+
+
+    }
+],
+sponsors:
+batchNo:
+facult:[
+    {
+        name:
+        dasignation:
+        qualification:
+        about:
+        contact:
+        email:
+    }
 ]
+ambassadors:[
+    "" , "",
+],
+duration:
+founder:
+fees:
+expenses:
+classes:
+lucation:
+mentors:
+ifeSkils:
+
+}
---
+++ script2.js
@@ -0,0 +1,33 @@
+let restuarent = [{
+    restuarentName: "kolachi",
+    address: "DHA phase 8karachi",
+    phoneNo: 8987543,
+    city: "karachi",
+},
+    menuList: {
+
+        dasyFood: {
+            chickenKarahi: "Rs 900",
+            muttonKarahi: "Rs 1200",
+        },
+        seaFood:
+        {
+            griledFish: "Rs 700",
+            freidFish: "Rs 600",
+        },
+        chineseFood: {
+            shashlik: "Rs 500",
+            freidRice: "Rs 300",
+            chineseVegetarianRice: "Rs 600",
+            machorian: "Rs 500",
+        },
+        fastFood: {
+            frunchFries: "Rs 200",
+        },
+    
+
+]
+
+
+
+
