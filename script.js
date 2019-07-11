4fa0900875b0f2eb9e1be6f66d622d55d9de5a29
faizashermuhammad <49517190+faizashermuhammad@users.noreply.github.com>
2019-07-11 16:04:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let student={
+    name:"hira",
+    email:"hira@gmail.com",
+    address:"nishter road",
+}
+
+let student2={
+    name:"samreen",
+    address: "garden",
+    email:"gdh@gmail.com",
+}
+
+let students=[
+    {
+        name:"amber",
+        addres: "garden",
+        age:19,
+        number:97654323 ,
+        email: "hgk@gail.com",
+    
+
+    name:"tuba",
+        addres: "garden",
+        age:19,
+        number:97654323 ,
+        email: "hgk@gail.com",
+}
+]
